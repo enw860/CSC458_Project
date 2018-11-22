e223f419fed4dad5857e496ef414e12bf79d525a
Lionel <lionel@Lionels-MacBook-Pro.local>
2018-11-22 02:27:05 -0500

    test frist part

---
+++ src2/analysis_perflow/analysis_perflow.R
@@ -0,0 +1,261 @@
+library(grid)
+library(gridExtra)
+library(ggplot2)
+library(varhandle)
+
+root_dir <- "/Users/lionel/Desktop/CSC458/Assignment/project_due_1125/src2/"
+
+################## begin default settings #####################
+source(paste(root_dir, "util/util.R", sep=""))
+
+data_dir <- set_filePath(root_dir, "data/") 
+img_dir <- set_filePath(root_dir, "plots/")
+################## end default settings #####################
+
+################## begin analysis overall functions #####################
+analysis_overall_tcp_flows <- function(tcps){
+  overall_tcp_flows <- NULL
+  flow_num <- as.integer(levels(factor(tcps$tcp.stream)))
+  
+  progress <- 0
+  for(flow in flow_num){
+    flow_set <- tcps[tcps$tcp.stream == flow, c(1:18)]
+    overall_tcp_flows <- rbind(overall_tcp_flows, analysis_tcp_flow(flow_set))
+    
+    if(as.integer(flow*100/max(flow_num)) > progress+2){
+      progress <- as.integer(flow*100/max(flow_num))
+      print(paste("Finish TCP %:", progress))
+    }
+  }
+  
+  return(overall_tcp_flows)
+}
+
+analysis_tcp_flow <- function(data){
+  # fix bit status
+  data$tcp.connection.fin[is.na(data$tcp.connection.fin)] <- 0
+  data$tcp.connection.rst[is.na(data$tcp.connection.rst)] <- 0
+  data$tcp.connection.syn[is.na(data$tcp.connection.syn)] <- 0
+  
+  # count connection duration
+  arrival_time <- convert_time(data$frame.time)
+  
+  tcp_flow <- data.frame(
+    Stream=data$tcp.stream[1],
+    Protocol=data$X_ws.col.Protocol[1],
+    dest=paste(data$ip.dst[1], data$tcp.dstport[1], sep = ":"),
+    source=paste(data$ip.src[1], data$tcp.srcport[1], sep = ":"),
+    total_hdr_size=sum(data$tcp.hdr_len + data$ip.hdr_len + 18),
+    total_bytes=sum(data$frame.len),
+    duration=tail(arrival_time, 1) - head(arrival_time, 1),
+    avg_inter_arriving=inter_arriving_time(arrival_time),
+    packets=length(arrival_time)
+  )
+  
+  # check the exit status
+  exit_state <- ""
+  if(tail(data$tcp.connection.fin, 1) == 1){
+    exit_state <- "FIN"
+  }else if(tail(data$tcp.connection.syn, 1) == 1){
+    exit_state <- "SYN"
+  }else if(tail(data$tcp.connection.rst, 1) == 1){
+    exit_state <- "RST"
+  }else{
+    # test if last packet within 5 minutes
+    if(tcp_flow$duration < 5*60){
+      exit_state <- "ONGOING"
+    }else{
+      exit_state <- "FAIL"
+    }
+  }
+  
+  tcp_flow$exit_state <- exit_state
+  tcp_flow$transfer_Bytes <- tcp_flow$total_bytes - tcp_flow$total_hdr_size
+  tcp_flow$transfer_Bytes[tcp_flow$transfer_Bytes == 0] <- 9999
+  tcp_flow$overhead_rate <- (tcp_flow$total_hdr_size*100)/ tcp_flow$transfer_Bytes
+  
+  return(tcp_flow)
+}
+
+analysis_overall_udp_flows <- function(udps){
+  overall_udp_flows <- NULL
+  flow_num <- as.integer(levels(factor(udps$udp.stream)))
+  
+  progress <- 0
+  for(flow in flow_num){
+    flow_set <- udps[udps$udp.stream == flow, c(1:10)]
+    overall_udp_flows <- rbind(overall_udp_flows, analysis_udp_flow(flow_set))
+    
+    if(as.integer(flow*100/max(flow_num)) > progress+5){
+      progress <- as.integer(flow*100/max(flow_num))
+      print(paste("Finish UDP %:", progress))
+    }
+  }
+  
+  return(overall_udp_flows)
+}
+
+analysis_udp_flow <- function(data){
+  # count connection duration
+  arrival_time <- convert_time(data$frame.time)
+  
+  udp_flow <- data.frame(
+    Stream=data$udp.stream[1],
+    Protocol=data$X_ws.col.Protocol[1],
+    dest=paste(data$ip.dst[1], data$udp.dstport[1], sep = ":"),
+    source=paste(data$ip.src[1], data$udp.srcport[1], sep = ":"),
+    total_hdr_size=sum(8 + data$ip.hdr_len + 18),
+    total_bytes=sum(data$frame.len),
+    duration=tail(arrival_time, 1) - head(arrival_time, 1),
+    avg_inter_arriving=inter_arriving_time(arrival_time),
+    packets=length(arrival_time)
+  )
+  
+  udp_flow$transfer_Bytes <- udp_flow$total_bytes - udp_flow$total_hdr_size
+  udp_flow$transfer_Bytes[udp_flow$transfer_Bytes == 0] <- 9999
+  udp_flow$overhead_rate <- (udp_flow$total_hdr_size*100)/ udp_flow$transfer_Bytes
+  
+  return(udp_flow)
+}
+
+inter_arriving_time <- function(arrival_time){
+  inter_arriving <- arrival_time[-1] - arrival_time[-1*length(arrival_time)]
+  return(mean(inter_arriving))
+}
+
+################## end analysis overall functions #####################
+
+################# start plot grapg ##################
+# flows statistics
+flows_statistics <- function(dir, tcp_flows, udp_flows){
+  tcp_count <- length(tcp_flows$packets)
+  udp_count <- length(udp_flows$packets)
+  
+  total_count <- tcp_count + udp_count
+  
+  tcp_stat <- data.frame(Protocol="TCP", Precentage=round(100*tcp_count/total_count, 2), FlowCount=tcp_count, TotalPackets=sum(tcp_flows$packets))
+  udp_stat <- data.frame(Protocol="UDP", Precentage=round(100*udp_count/total_count, 2), FlowCount=udp_count, TotalPackets=sum(udp_flows$packets))
+  
+  df <- rbind(tcp_stat, udp_stat)
+  
+  png(set_filePath(dir, "flows_statistics.png"), height=100, width=400)
+  grid.arrange(
+    make_table(df, "Flows Statistics")
+  )
+  dev.off()
+}
+
+duration_cdfs <- function(dir ,tcp_flows, udp_flows){
+  x_label <- "flows duration(ms)"
+  
+  tcp_duration <- tcp_flows$duration
+  udp_duration <- udp_flows$duration
+  total_duration <- c(tcp_duration, udp_duration)
+  
+  png(set_filePath(dir, "durations_cdfs.png"), height=800, width=1000)
+  
+  par(mfrow=c(2,2))
+  cdf(total_duration, "Total flows duration cdf", x_label)
+  cdf(tcp_duration, "TCP flows duration cdf", x_label)
+  cdf(udp_duration, "UDP flows duration cdf", x_label)
+  dev.off()
+}
+
+byteSum_cdfs <- function(dir ,tcp_flows, udp_flows){
+  x_label <- "flows bytes"
+  
+  tcp_bytes <- tcp_flows$total_bytes
+  udp_bytes <- udp_flows$total_bytes
+  total_bytes <- c(tcp_bytes, udp_bytes)
+  
+  png(set_filePath(dir, "bytes_cdfs.png"), height=800, width=1000)
+  
+  par(mfrow=c(2,2))
+  cdf(total_bytes, "Total flows bytes cdf", x_label)
+  cdf(tcp_bytes, "TCP flows bytes cdf", x_label)
+  cdf(udp_bytes, "UDP flows bytes cdf", x_label)
+  dev.off()
+}
+
+packetCount_cdfs <- function(dir ,tcp_flows, udp_flows){
+  x_label <- "flows packets count"
+  
+  tcp_count <- tcp_flows$packets
+  udp_count <- udp_flows$packets
+  total_count <- c(tcp_count, udp_count)
+  
+  png(set_filePath(dir, "count_cdfs.png"), height=800, width=1000)
+  
+  par(mfrow=c(2,2))
+  cdf(total_count, "Total flows packets count cdf", x_label)
+  cdf(tcp_count, "TCP flows packets count cdf", x_label)
+  cdf(udp_count, "UDP flows packets count cdf", x_label)
+  dev.off()
+}
+
+overhead_cdfs <- function(dir ,tcp_flows){
+  x_label <- "overhead rate %"
+  
+  tcp_overhead_rate <- tcp_flows$overhead_rate
+  
+  png(set_filePath(dir, "overhead_cdfs.png"), height=800, width=1000)
+  par(mfrow=c(1,1))
+  cdf(tcp_overhead_rate, "TCP overhead rate cdf", x_label)
+  dev.off()
+}
+
+inter_packet_arrival_cdfs <- function(dir ,tcp_flows, udp_flows){
+  x_label <- "avgrage inter arriving time (s)"
+  
+  tcp_avg_inter_arriving <- tcp_flows$avg_inter_arriving[!is.na(tcp_flows$avg_inter_arriving)]
+  udp_avg_inter_arriving <- udp_flows$avg_inter_arriving[!is.na(udp_flows$avg_inter_arriving)]
+  
+  png(set_filePath(dir, "avg_inter_arriving.png"), height=800, width=1000)
+  par(mfrow=c(1,2))
+  cdf(tcp_avg_inter_arriving, "TCP avgrage inter arriving time", x_label)
+  cdf(udp_avg_inter_arriving, "UDP avgrage inter arriving time", x_label)
+  dev.off()
+}
+
+exit_state_statistics <- function(img_dir, tcp_flows){
+  len <- length(tcp_flows$exit_state)
+  count_tb <- table(tcp_flows$exit_state)
+  
+  types <- names(count_tb)
+  count <- rep(unname(count_tb))
+  precenatage <- round(count*100/len, 2)
+  
+  stats <- data.frame(types=types, precenatage=precenatage, count=count)
+
+  png(set_filePath(img_dir, "exit_state_statistics.png"), height=100, width=400)
+  par(mfrow=c(1,1))
+  grid.arrange(
+    make_table(stats, "Exit State Statistics")
+  )
+  dev.off()
+}
+################# end plot grapg ##################
+# loading data (~ 3 mins)
+tcps <- read.table(set_filePath(data_dir, "tcp_result.tsv"), header=TRUE, sep="\t")
+udps <- read.table(set_filePath(data_dir, "udp_result.tsv"), header=TRUE, sep="\t")
+
+# analysis data (~ 5 mins)
+overall_tcp_flows <- analysis_overall_tcp_flows(tcps)
+overall_udp_flows <- analysis_overall_udp_flows(udps)
+
+# per flow bullet point 1
+flows_statistics(img_dir ,overall_tcp_flows, overall_udp_flows)
+
+# per flow bullet point 2
+duration_cdfs(img_dir ,overall_tcp_flows, overall_udp_flows)
+
+# per flow bullet point 3
+byteSum_cdfs(img_dir ,overall_tcp_flows, overall_udp_flows)
+packetCount_cdfs(img_dir ,overall_tcp_flows, overall_udp_flows)
+overhead_cdfs(img_dir, overall_tcp_flows)
+
+# per flow bullet point 4
+inter_packet_arrival_cdfs(img_dir ,overall_tcp_flows, overall_udp_flows)
+
+# per flow bullet point 5
+exit_state_statistics(img_dir, overall_tcp_flows)
---
+++ src2/analysis_perpack/analysis_perpack.R
@@ -0,0 +1,247 @@
+library(grid)
+library(gridExtra)
+library(ggplot2)
+
+root_dir <- "/Users/lionel/Desktop/CSC458/Assignment/project_due_1125/src/"
+
+# default setting (set your own absoulte path)
+source(paste(root_dir, "util/util.R", sep=""))
+
+data_dir <- set_filePath(root_dir, "data/") 
+img_dir <- set_filePath(root_dir, "plots/")
+
+other_net_protocol <- c("MS NLB", "LLC", "IPX", "Intel ANS probe")
+other_trans_protocol <- c("VRRP", "PIMv0", "OSPF", "NCS", "IGMPv0", "GRE", "ESP", "IGRP")
+
+x_label <- "packet length"
+y_label <- "cummulate probability"
+######################### start control box #####################
+plot_scatter <- bool_switch(0)
+plot_ecdf <- bool_switch(1)
+plot_cdf <- bool_switch(1)
+######################### end control box #####################
+
+# plot precentage tables
+plot_precentage_tables <- function(data, dir){
+  trans_stat <- transport_statistics(data)
+  net_stat <- network_statistics(data, trans_stat)
+  link_stat <- link_statistics(data, net_stat)
+  
+  # plot tables
+  png(set_filePath(dir, "packets_precentage_statistics.png"), height=350, width=400)
+  grid.arrange(
+    make_table(link_stat, "Link Layer Packets Statistics"),
+    make_table(net_stat, "Network Layer Packets Statistics"),
+    make_table(trans_stat, "Transport Layer Packets Statistics")
+  )
+  dev.off()
+}
+
+# count packets and size of packets in Transport Layer
+transport_statistics <- function(data){
+  tcp_filter <- data$frame.len[data$X_ws.col.Protocol=="TCP"]
+  udp_filter <- data$frame.len[data$X_ws.col.Protocol=="UDP"]
+  other_filter <- data$frame.len[data$X_ws.col.Protocol %in% other_trans_protocol]
+  
+  tcp_count <- length(tcp_filter)
+  udp_count <- length(udp_filter)
+  other_count <- length(other_filter)
+  
+  total_count <- tcp_count + udp_count + other_count
+  
+  tcp_stat <- data.frame(Protocol="TCP",Precentage=round(100*tcp_count/total_count, 2),PackageCount=tcp_count,TotalSize=sum(tcp_filter))
+  udp_stat <- data.frame(Protocol="UDP",Precentage=round(100*udp_count/total_count, 2),PackageCount=udp_count,TotalSize=sum(udp_filter))
+  other_stat <- data.frame(Protocol="Other",Precentage=round(100*other_count/total_count, 2),PackageCount=other_count,TotalSize=sum(other_filter))
+  
+  df <- rbind(tcp_stat, udp_stat)
+  df <- rbind(df, other_stat)
+  
+  return(df)
+}
+
+# count packets and size of packets in Network Layer
+network_statistics <- function(data, transLayer){
+  ip_filter <- data$frame.len[data$X_ws.col.Protocol %in% c("IPv4", "IPv6")]
+  icmp_filter <- data$frame.len[data$X_ws.col.Protocol %in% c("ICMP", "ICMPv6")]
+  other_filter <- data$frame.len[data$X_ws.col.Protocol %in% other_net_protocol]
+  
+  ip_count <- length(ip_filter) + sum(transLayer$PackageCount)
+  icmp_count <- length(icmp_filter)
+  other_count <- length(other_filter)
+  
+  total_count <- ip_count + icmp_count + other_count
+  
+  ip_stat <- data.frame(Protocol="IP",Precentage=round(100*ip_count/total_count, 2),PackageCount=ip_count,TotalSize=sum(ip_filter)+sum(transLayer$TotalSize))
+  icmp_stat <- data.frame(Protocol="ICMP",Precentage=round(100*icmp_count/total_count, 2),PackageCount=icmp_count,TotalSize=sum(icmp_filter))
+  other_stat <- data.frame(Protocol="Other",Precentage=round(100*other_count/total_count, 2),PackageCount=other_count,TotalSize=sum(other_filter))
+  
+  df <- rbind(ip_stat, icmp_stat)
+  df <- rbind(df, other_stat)
+  
+  return(df)
+}
+
+# count packets and size of packets in Link Layer
+link_statistics <- function(data, netLayer){
+  arp_filter <- data$frame.len[data$X_ws.col.Protocol == "ARP"]
+  
+  arp_count <- length(arp_filter) 
+  E802q_count <- sum(netLayer$PackageCount)
+  
+  total_count <- arp_count + E802q_count
+  
+  E802q_stat <- data.frame(Protocol="Ethernet",Precentage=round(100*E802q_count/total_count, 2),PackageCount=E802q_count,TotalSize=sum(netLayer$TotalSize))
+  arp_stat <- data.frame(Protocol="ARP",Precentage=round(100*arp_count/total_count, 2),PackageCount=arp_count,TotalSize=sum(arp_filter))
+  
+  df <- rbind(E802q_stat, arp_stat)
+  
+  return(df)
+}
+
+# plots for packet length analysis for bullet point 2
+plot_total_packlen_cdf <- function(data, ip_data, non_ip_data, dir){
+  # scatter plot
+  if(plot_scatter){
+    png(set_filePath(dir, "packlen_total_scatter.png"), height=800, width=1000)
+    
+    par(mfrow=c(1,1))
+    plot(data$frame.len[!is.na(data$frame.len)], xlab="index",  ylab="packet size", main="Total length scatter")
+    dev.off();
+  }
+  
+  # ecdf plot
+  if(plot_ecdf){
+    png(set_filePath(dir, "packlen_total_ecdf.png"), height=800, width=1000)
+    
+    par(mfrow=c(1,1))
+    total_len_ecdf <- ecdf(data$frame.len[!is.na(data$frame.len)])
+    plot(total_len_ecdf, xlab=x_label, ylab=y_label, main="Total length ecdf")
+    dev.off();
+  }
+  
+  # cdf plot
+  if(plot_cdf){
+    png(set_filePath(dir, "packlen_total_cdf.png"), height=800, width=1000)
+    
+    par(mfrow=c(1,1))
+    cdf(data$frame.len, "Total length cdf")
+    dev.off();
+  }
+  
+  tcp_data <- ip_data$frame.len[ip_data$Protocol=="TCP"]
+  udp_data <- ip_data$frame.len[ip_data$Protocol=="UDP"]
+  
+  # scatter plots
+  if(plot_scatter){
+    png(set_filePath(dir, "packlen_scatter.png"), height=800, width=1000)
+    par(mfrow=c(2,2))
+    plot(ip_data$frame.len[!is.na(ip_data$frame.len)], xlab="index",  ylab="packet size", main="IP Packet Length scatter")
+    plot(non_ip_data$frame.len[!is.na(non_ip_data$frame.len)],  xlab="index",  ylab="packet size", main="non-IP Packet Length scatter")
+    plot(tcp_data[!is.na(tcp_data)], xlab="index",  ylab="packet size", main="TCP Packet Length scatter")
+    plot(udp_data[!is.na(udp_data)], xlab="index",  ylab="packet size", main="UDP Packet Length scatter")
+    dev.off()
+  }
+  
+  # ecdf plots
+  if(plot_ecdf){
+    png(set_filePath(dir, "packlen_ecdfs.png"), height=800, width=1000)
+    
+    ip_len_ecdf <- ecdf(ip_data$frame.len[!is.na(ip_data$frame.len)])
+    non_ip_len_ecdf <- ecdf(non_ip_data$frame.len[!is.na(non_ip_data$frame.len)])
+    tcp_len_ecdf <- ecdf(tcp_data[!is.na(tcp_data)])
+    udp_len_ecdf <- ecdf(udp_data[!is.na(udp_data)])
+  
+    par(mfrow=c(2,2))
+    plot(ip_len_ecdf, xlab=x_label, ylab=y_label, main="IP Packet Length ecdf")
+    plot(non_ip_len_ecdf, xlab=x_label, ylab=y_label, main="non-IP Packet Length ecdf")
+    plot(tcp_len_ecdf, xlab=x_label, ylab=y_label, main="TCP Packet Length ecdf")
+    plot(udp_len_ecdf, xlab=x_label, ylab=y_label, main="UDP Packet Length ecdf")
+    dev.off()
+  }
+  
+  # cdf plots
+  if(plot_cdf){
+    png(set_filePath(dir, "packlen_cdfs.png"), height=800, width=1000)
+    
+    par(mfrow=c(2,2))
+    cdf(ip_data$frame.len, "IP Packet Length cdf")
+    cdf(non_ip_data$frame.len, "Non_IP Packet Length cdf")
+    cdf(tcp_data, "TCP Packet Length cdf")
+    cdf(udp_data, "UDP Packet Length cdf")
+    dev.off()
+  }
+}
+
+# plots for header length analysis for bullet point 2
+plot_total_headerlen_cdf <- function(data, dir){
+  # scatter plots
+  if(plot_scatter){
+    png(set_filePath(img_dir, "header_length_scatter.png"), height=800, width=1000)
+  
+    par(mfrow=c(2,2))
+    plot(data$ip.header[!is.na(data$ip.header)], xlab="index",  ylab="packet size", main="IP header scatter")
+    plot(data$tcp.header[!is.na(data$tcp.header)], xlab="index",  ylab="packet size", main="TCP packet header scatter")
+    plot(data$udp.header[!is.na(data$udp.header)], xlab="index",  ylab="packet size", main="UDP packet header scatter")
+    dev.off()
+  }
+  
+  # ecdf plots
+  if(plot_ecdf){
+    png(set_filePath(img_dir, "header_length_ecdfs.png"), height=800, width=1000)
+    
+    ip_header_ecdf <- ecdf(data$ip.header[!is.na(data$ip.header)])
+    tcp_header_ecdf <- ecdf(data$tcp.header[!is.na(data$tcp.header)])
+    udp_header_ecdf <- ecdf(data$udp.header[!is.na(data$udp.header)])
+    
+    par(mfrow=c(2,2))
+    plot(ip_header_ecdf, xlab=x_label, ylab=y_label, main="IP packet header cdf")
+    plot(tcp_header_ecdf, xlab=x_label, ylab=y_label, main="TCP packet header cdf")
+    plot(udp_header_ecdf, xlab=x_label, ylab=y_label, main="UDP packet header cdf")
+    dev.off()
+  }
+  
+  # cdf plots
+  if(plot_cdf){
+    png(set_filePath(img_dir, "header_length_cdfs.png"), height=800, width=1000)
+    
+    par(mfrow=c(2,2))
+    cdf(data$ip.header, "IP packet header cdf")
+    cdf(data$tcp.header, "TCP packet header cdf")
+    cdf(data$udp.header, "UDP packet header cdf")
+    dev.off()
+  }
+}
+
+# Load data
+data <- read.table(set_filePath(data_dir, "result.tsv"), sep = "\t" , header = TRUE, stringsAsFactors = FALSE)
+
+data$ip.hdr_len <- suppressWarnings(as.integer(data$ip.hdr_len))
+data$ip.len <- suppressWarnings(as.integer(data$ip.len))
+
+ip_filter <- data$X_ws.col.Protocol %in% c('TCP', 'UDP', 'IPv4', 'IPv6')
+ip_data <- data.frame(
+  Protocol=data$X_ws.col.Protocol[ip_filter],
+  frame.len=data$frame.len[ip_filter],
+  frame.cap_len=data$frame.cap_len[ip_filter],
+  ip.total_len=data$ip.len[ip_filter],
+  ip.header=data$ip.hdr_len[ip_filter],
+  tcp.header=data$tcp.hdr_len[ip_filter],
+  tcp.seg_len=data$tcp.len[ip_filter],
+  udp.header=rep(8, sum(ip_filter)),
+  udp.total_len=data$udp.length[ip_filter]
+)
+
+non_ip_data <- data.frame(
+  Protocol=data$X_ws.col.Protocol[data$X_ws.col.Protocol %in% other_net_protocol],
+  frame.len=data$frame.len[data$X_ws.col.Protocol %in% other_net_protocol],
+  frame.cap_len=data$frame.cap_len[data$X_ws.col.Protocol %in% other_net_protocol],
+  ip.total_len=data$ip.len[data$X_ws.col.Protocol %in% other_net_protocol],
+  ip.header=data$ip.hdr_len[data$X_ws.col.Protocol %in% other_net_protocol]
+)
+
+# plot per-packet analysis bullet point 1
+plot_precentage_tables(data, img_dir)
+
+# plot per-packet analysis bullet point 2
+plot_total_packlen_cdf(data, ip_data, non_ip_data, img_dir)
+plot_total_headerlen_cdf(ip_data, img_dir)
